f27b476e65a88c20b096d349978cecca3f73c7b5
rakitha beminiwattha <rakithab@gmail.com>
2015-02-15 17:12:41 -0500

    completed two routines for caching the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Caching the inverse of a matrix
 ## functions do
 
-## makeVector creates a special "matrix", which is really a list containing a function to
+## makeCacheMatrix creates a special "matrix", which is really a list containing a function to
 ## 
 ## 1. set the value of the matrix
 ## 2. get the value of the matrix
@@ -22,7 +22,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## calculates the inverse of the special "matrix" created with the above function.
+## cacheSolve calculates the inverse of the special "matrix" created with the above function.
 ## However, it first checks to see if the inverse has already been calculated. 
 ## If so, it gets the inverse from the cache and skips the computation. 
 ## Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setSolve function.
